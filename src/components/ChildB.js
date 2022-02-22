52e15d3fa4735e6123d937d08d3bbdc805de9c73
TingYun <tingyun02@hotmail.com>
2022-02-23 00:04:03 +0800

    Props (子女>父母)

---
+++ src/components/ChildB.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+function ChildB(props) {
+  const { setData } = props
+  const [childBData, setChildBData] = useState('ChildB')
+  return (
+    <>
+      <h2>ChildB</h2>
+      <button
+        onClick={() => {
+          setData(childBData)
+        }}
+      >
+        Click
+      </button>
+    </>
+  )
+}
+
+export default ChildB
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,13 +1,18 @@
 // import Child from './Child'
 import { useState } from 'react'
 import ChildA from './ChildA'
+import ChildB from './ChildB'
 
 function Parent() {
   const [pData, setPData] = useState('Parent')
 
+  const [data, setData] = useState('')
+  // 給ChildB回傳資料用的狀態，透過callback function傳給Parent
   return (
     <>
       <h2>Parent</h2>
+      <p>來自ChildB的資料: {data}</p>
+      <ChildB setData={setData} />
       <ChildA pData={pData} />
     </>
   )
