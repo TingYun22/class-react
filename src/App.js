5afca91a720d23ec20e5923e614f891aa5399496
TingYun <tingyun02@hotmail.com>
2022-02-24 00:15:26 +0800

    建資料

--- src/App.js
+++ src/App.js
@@ -12,13 +12,20 @@ const products = [
     image: 'https://i.imgur.com/1GrakTl.jpg',
     price: 300,
   },
-  //   {
-  //     id: 2,
-  //     name: '白色 T-shirt',
-  //     categroy: 'Shirt',
-  //     image: 'https://i.imgur.com/ba3tvGm.jpg',
-  //     price: 200,
-  //   },
+  {
+    id: 2,
+    name: '白色 T-shirt',
+    categroy: 'Shirt',
+    image: 'https://i.imgur.com/ba3tvGm.jpg',
+    price: 200,
+  },
+  {
+    id: 3,
+    name: '黑色 T-shirt',
+    categroy: 'Shirt',
+    image: 'https://i.imgur.com/pHQ3xT3.jpg',
+    price: 450,
+  },
 ]
 
 function App() {
